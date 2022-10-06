4e2ecd0bb923b1f1d781499302946e807824af9c
Dmitry White <dmitry.b.white@gmail.com>
2022-10-06 11:49:34 +0100

    Add variable declaration

--- udemy/cards/main.go
+++ udemy/cards/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+// Error: expected declaration, found name
+// name := "Dmitry"
+
+var name string = "Dmitry"
+
+func main() {
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"
+	card = "Ace of Hearts"
+
+	fmt.Println(card)
 }
