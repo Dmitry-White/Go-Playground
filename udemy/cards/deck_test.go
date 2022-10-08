9a47355f732e4bfa1eebabbe5b1d70cbb2f09263
Dmitry White <dmitry.b.white@gmail.com>
2022-10-08 20:40:12 +0100

    Add slice tests

--- udemy/cards/deck_test.go
+++ udemy/cards/deck_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestNewDeck(t *testing.T) {
-	cards := newCard()
-	deckLength := len(cards)
+	cards := newDeck()
 
-	if deckLength != 13 {
-		t.Errorf("Expected deck length of 13, but got %v", deckLength)
+	expectedDeckLength := 16
+	actualDeckLength := len(cards)
+	if actualDeckLength != expectedDeckLength {
+		t.Errorf("Expected deck length of %v, but got %v", expectedDeckLength, actualDeckLength)
+	}
+
+	expectedFirstCard := "Ace of Spades"
+	actualFirstCard := cards[0]
+	if actualFirstCard != expectedFirstCard {
+		t.Errorf("Expected first card to be %v, but got %v", expectedFirstCard, actualFirstCard)
+	}
+
+	expectedLastCard := "Four of Clubs"
+	actualLastCard := cards[len(cards)-1]
+	if actualLastCard != expectedLastCard {
+		t.Errorf("Expected last card to be %v, but got %v", expectedLastCard, actualLastCard)
 	}
 }
