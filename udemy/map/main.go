5ff60af1328a15e9c79f889879ec7b45c28b5459
Dmitry White <dmitry.b.white@gmail.com>
2022-10-10 22:29:09 +0100

    Add a declared map

--- udemy/map/main.go
+++ udemy/map/main.go
@@ -11,6 +11,22 @@ func createLiteralMap() {
 	fmt.Println(colours)
 }
 
+func createDeclaredMap() {
+	// var colours map[string]string
+	colours := make(map[string]string)
+
+	// Because maps in Go are statically typed
+	// and the key in map should always be of certain type
+	// "dot notation" property access is not available
+	colours["red"] = "#ff0000"
+	colours["green"] = "#4bf745"
+
+	delete(colours, "green")
+
+	fmt.Println(colours)
+}
+
 func main() {
 	createLiteralMap()
+	createDeclaredMap()
 }
