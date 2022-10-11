77f827feeb6ebd0735e160a3d99967ab6d884f9f
Dmitry White <dmitry.b.white@gmail.com>
2022-10-11 15:48:35 +0100

    Add byte slice to be passed to Reader.Read

--- udemy/http/main.go
+++ udemy/http/main.go
@@ -12,4 +12,14 @@ func main() {
 	}
 
 	log.Printf("%+v\n", *resp)
+
+	// Allocating memory beforehand
+	// since Reader.Read function can't automatically
+	// resize the slice if it's already full
+	// therefore if passed an empty slice
+	// it won't be able to fill it with data
+	data := make([]byte, 99999)
+	resp.Body.Read(data)
+
+	log.Println(string(data))
 }
