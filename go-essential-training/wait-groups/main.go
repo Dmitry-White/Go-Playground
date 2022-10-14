58229a4a5f9e12acd61c6e736ffc70a367a51569
Dmitry White <dmitry.b.white@gmail.com>
2022-10-14 22:19:07 +0100

    Add fetching scaffolding

--- go-essential-training/wait-groups/main.go
+++ go-essential-training/wait-groups/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func fetchSerial(urls []string) {
+	fmt.Println("----------------")
+	fmt.Println("Fetch Serial")
+	fmt.Println(urls)
+	fmt.Println("----------------")
+}
+
+func fetchConcurrent(urls []string) {
+	fmt.Println("----------------")
+	fmt.Println("Fetch Concurrent")
+	fmt.Println(urls)
+	fmt.Println("----------------")
+}
 
 func main() {
 	fmt.Println("Wait Groups")
+
+	urls := []string{
+		"https://golang.org",
+		"https://api.github.com",
+		"https://google.com",
+		"https://amazon.com",
+	}
+
+	startSerial := time.Now()
+	fetchSerial(urls)
+	endSerial := time.Since(startSerial)
+	fmt.Println("Time Elapsed: ", endSerial)
+
+	startConcurrent := time.Now()
+	fetchConcurrent(urls)
+	endConcurrent := time.Since(startConcurrent)
+	fmt.Println("Time Elapsed: ", endConcurrent)
 }
