96fd1a2713da856b5e2000b48d3ab0a7bb5ce850
Dmitry White <dmitry.b.white@gmail.com>
2022-10-17 20:19:04 +0100

    Add marshalResponse implementation

--- go-essential-training/json/marshaling.go
+++ go-essential-training/json/marshaling.go
@@ -19,3 +19,22 @@ func unmarshalRequest() {
 
 	log.Printf("Request: %+v\n", request)
 }
+
+func marshalResponse(amount float64) {
+	// Loaded from a DB
+	const currentBalance = 10000.0
+
+	// Marshal response
+	response := Response{
+		Ok:      true,
+		Balance: currentBalance + amount,
+	}
+
+	responseBytes, err := json.Marshal(response)
+
+	if err != nil {
+		log.Fatalf("error: can't marshal - %s", err)
+	}
+
+	log.Printf("Response: %+v\n", string(responseBytes))
+}
