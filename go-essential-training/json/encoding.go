1657ec3eb0ee809ed7430e3263d44aa3bab14730
Dmitry White <dmitry.b.white@gmail.com>
2022-10-17 19:25:52 +0100

    Add decodeRequest implementation

--- go-essential-training/json/encoding.go
+++ go-essential-training/json/encoding.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"strings"
 )
 
 func decodeRequest() {
-	log.Println("Not Implemented")
+	// Simulate a file/socket
+	requestReader := strings.NewReader(DATA)
+
+	// Create request decoder
+	requestDecoder := json.NewDecoder(requestReader)
+
+	// Decode request
+	var request Request
+	err := requestDecoder.Decode(&request)
+
+	if err != nil {
+		log.Fatalf("error: can't decode - %s", err)
+	}
+
+	log.Printf("Request: %+v\n", request)
 }
