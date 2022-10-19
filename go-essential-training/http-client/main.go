89923232a62f14c1f620b6357cd8a911f460e27d
Dmitry White <dmitry.b.white@gmail.com>
2022-10-19 22:43:48 +0100

    Add addUser implmentation

--- go-essential-training/http-client/main.go
+++ go-essential-training/http-client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -44,9 +45,42 @@ func getUser(id string) User {
 	return user
 }
 
+func addUser(user User) map[string]interface{} {
+	url := fmt.Sprintf("%s/%s", API["BASE_URL"], API["USERS"])
+
+	userBytes, jsonMarshalErr := json.Marshal(user)
+	if jsonMarshalErr != nil {
+		log.Fatalf("error: can't marshal - %s", jsonMarshalErr)
+	}
+
+	bytesReader := bytes.NewReader(userBytes)
+
+	response, err := http.Post(url, "application/json", bytesReader)
+	if err != nil {
+		log.Fatalf("Error: can't call %s, results in %v", API["BASE_URL"], err)
+	}
+
+	defer response.Body.Close()
+
+	buffer := make([]byte, 9999)
+	length, _ := response.Body.Read(buffer)
+
+	var data map[string]interface{}
+	jsonUnmarshalErr := json.Unmarshal(buffer[:length], &data)
+	if jsonUnmarshalErr != nil {
+		log.Fatalf("Error: can't unmarshal %v", jsonUnmarshalErr)
+	}
+
+	return data
+}
+
 func main() {
 	getUsers()
 
 	user := getUser("1")
 	log.Printf("User: %+v", user)
+
+	newUser := User{Name: "Test", Username: "TestUsername", Email: "test@test.com"}
+	data := addUser(newUser)
+	log.Printf("Data: %+v", data)
 }
