657a7df24fbf03a7fd1a14bdb8f4209b9113f3d0
Dmitry White <dmitry.b.white@gmail.com>
2022-11-09 15:18:08 +0000

    Add naive implementation for conversation

--- system-tooling-with-go/packages/core/constants.go
+++ system-tooling-with-go/packages/core/constants.go
@@ -1,6 +1,11 @@
 package core
 
 var Names = map[string]string{
-	"friend": "General Kenobi",
-	"enemy":  "General Grievous",
+	"friend": "Kenobi",
+	"enemy":  "Grievous",
+}
+
+var Messages = map[string]string{
+	"friend": "Oh hello there!",
+	"enemy":  "General Kenobi.",
 }
--- system-tooling-with-go/packages/main.go
+++ system-tooling-with-go/packages/main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"packages/core"
 	"packages/utils"
+	"strings"
 )
 
 func main() {
-	friendName := core.Names["friend"]
-	friendSays := utils.Greeting(friendName, "Oh hello there!")
-	fmt.Println(friendSays)
+	reader := bufio.NewReader(os.Stdin)
 
-	enemyName := core.Names["enemy"]
-	enemySays := utils.Greeting(enemyName, friendName)
-	fmt.Println(enemySays)
+	fmt.Print("Enter 1st person: ")
+	firstPersonEntry, _ := reader.ReadString('\n')
+	trimmedFirstPersonEntry := strings.TrimSpace(firstPersonEntry)
+	firstPersonName := core.Names[trimmedFirstPersonEntry]
+	firstPersonMessage := core.Messages[trimmedFirstPersonEntry]
+	firstPersonSays := utils.Greeting(firstPersonName, firstPersonMessage)
+
+	fmt.Print("Enter 2nd person: ")
+	secondPersonEntry, _ := reader.ReadString('\n')
+	trimmedSecondPersonEntry := strings.TrimSpace(secondPersonEntry)
+	secondPersonName := core.Names[trimmedSecondPersonEntry]
+	secondPersonMessage := core.Messages[trimmedSecondPersonEntry]
+	secondPersonSays := utils.Greeting(secondPersonName, secondPersonMessage)
+
+	fmt.Println("Your conversation:")
+	fmt.Println(firstPersonSays)
+	fmt.Println(secondPersonSays)
 }
