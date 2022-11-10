7134127e172cab1e1d62096810fd6ea9febee1eb
Dmitry White <dmitry.b.white@gmail.com>
2022-11-10 21:59:59 +0000

    Add RecordLineQuick implementation

--- system-tooling-with-go/packages/utils/files.go
+++ system-tooling-with-go/packages/utils/files.go
@@ -1,9 +1,17 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"packages/core"
+)
 
 func RecordLineQuick(line string) {
-	fmt.Println("Not Implemented")
+	err := os.WriteFile(core.FILE_NAME, []byte(line), 0644)
+	if err != nil {
+		log.Fatalf("Error: Failed to write to %s, %v", core.FILE_NAME, err)
+	}
 }
 
 func RecordLine(line string) {
