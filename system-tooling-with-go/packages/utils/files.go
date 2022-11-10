23c3b1f418377599bbe2b8128afff7c10b83defe
Dmitry White <dmitry.b.white@gmail.com>
2022-11-10 22:13:32 +0000

    Add RecordLine implementation

--- system-tooling-with-go/packages/utils/files.go
+++ system-tooling-with-go/packages/utils/files.go
@@ -8,14 +8,27 @@ import (
 )
 
 func RecordLineQuick(line string) {
-	err := os.WriteFile(core.FILE_NAME, []byte(line), 0644)
+	data := []byte(fmt.Sprintf("%s\n", line))
+
+	err := os.WriteFile(core.FILE_NAME, data, 0644)
 	if err != nil {
 		log.Fatalf("Error: Failed to write to %s, %v", core.FILE_NAME, err)
 	}
 }
 
 func RecordLine(line string) {
-	fmt.Println("Not Implemented")
+	data := []byte(fmt.Sprintf("%s\n", line))
+
+	file, err := os.OpenFile(core.FILE_NAME, os.O_APPEND, 0644)
+	if err != nil {
+		log.Fatalf("Error: Failed to open %s, %v", core.FILE_NAME, err)
+	}
+	defer file.Close()
+
+	_, err = file.Write(data)
+	if err != nil {
+		log.Fatalf("Error: Failed to write to %s, %v", core.FILE_NAME, err)
+	}
 }
 
 func PullRecords() {
