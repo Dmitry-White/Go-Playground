b2213ee5d7266ddd7b4cb134c3f8b980d71fe9cd
Dmitry White <dmitry.b.white@gmail.com>
2022-11-11 23:24:42 +0000

    Add handleWrite implementation

--- system-tooling-with-go/basic-cli/main.go
+++ system-tooling-with-go/basic-cli/main.go
@@ -11,25 +11,27 @@ import (
 type Flags struct {
 	name     string
 	greeting string
-	promt    bool
+	prompt   bool
 	preview  bool
 }
+
+const FILE_NAME = "conversation.txt"
 
 func parseFlags(f *Flags) {
 	flag.StringVar(&f.name, "name", "123", "name to use within the message")
 	flag.StringVar(&f.greeting, "greeting", "", "phrase to use within the message")
-	flag.BoolVar(&f.promt, "promt", false, "use prompt to input name and greeting")
-	flag.BoolVar(&f.preview, "preview", false, "use preview to output message without writing to conversation.txt")
+	flag.BoolVar(&f.prompt, "prompt", false, "use prompt to input name and greeting")
+	flag.BoolVar(&f.preview, "preview", false, "use preview to output message without writing to a file")
 
 	flag.Parse()
 }
 
 func showUsage(f *Flags) {
 	// Note:
-	// f.promt is a syntactic sugar for
+	// f.prompt is a syntactic sugar for
 	// (*f).name
 	// to reduce typing
-	if !f.promt && (f.name == "" || f.greeting == "") {
+	if !f.prompt && (f.name == "" || f.greeting == "") {
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -39,21 +41,23 @@ func handleDebug(f *Flags) {
 	if os.Getenv("DEBUG") != "" {
 		fmt.Println("Name: ", f.name)
 		fmt.Println("Greeting: ", f.greeting)
-		fmt.Println("Prompt: ", f.promt)
+		fmt.Println("Prompt: ", f.prompt)
 		fmt.Println("Preview: ", f.preview)
 	}
 }
 
 func handlePrompt(f *Flags) {
-	reader := bufio.NewReader(os.Stdin)
+	if f.prompt {
+		reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print("Your Name: ")
-	inputName, _ := reader.ReadString('\n')
-	f.name = strings.TrimSpace(inputName)
+		fmt.Print("Your Name: ")
+		inputName, _ := reader.ReadString('\n')
+		f.name = strings.TrimSpace(inputName)
 
-	fmt.Print("Your Greeting: ")
-	inputGreeting, _ := reader.ReadString('\n')
-	f.greeting = strings.TrimSpace(inputGreeting)
+		fmt.Print("Your Greeting: ")
+		inputGreeting, _ := reader.ReadString('\n')
+		f.greeting = strings.TrimSpace(inputGreeting)
+	}
 }
 
 func handlePreview(message string) {
@@ -61,11 +65,17 @@ func handlePreview(message string) {
 }
 
 func handleWrite(message string) {
-	fmt.Println("Not Implemented")
+	data := []byte(fmt.Sprintf("%s\n", message))
+
+	err := os.WriteFile(FILE_NAME, data, 0644)
+	if err != nil {
+		fmt.Printf("Error: Failed to write: %v", err)
+		os.Exit(1)
+	}
 }
 
 func handleMessage(f *Flags) {
-	message := ""
+	message := fmt.Sprintf("%s, %s", f.greeting, f.name)
 
 	if f.preview {
 		handlePreview(message)
