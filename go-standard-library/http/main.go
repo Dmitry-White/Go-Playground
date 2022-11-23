2857e0b84e9f0a0c0bb5c40bc31d8dd52ae494f7
Dmitry White <dmitry.b.white@gmail.com>
2022-11-23 15:39:22 +0000

    Add urls setup

--- go-standard-library/http/main.go
+++ go-standard-library/http/main.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Not Implemented")
+	fmt.Println("------------ URLs ------------")
+	urls()
+	fmt.Println("------------------------------")
 }
