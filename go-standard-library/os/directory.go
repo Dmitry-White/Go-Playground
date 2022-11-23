753d89c49ca5e5d262a568776c764522cd9c4326
Dmitry White <dmitry.b.white@gmail.com>
2022-11-23 13:00:06 +0000

    Add directory implementation

--- go-standard-library/os/directory.go
+++ go-standard-library/os/directory.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func directory() {
-	fmt.Println("Not Implemented")
+	// TODO: Create a directory
+	os.Mkdir("utils", os.ModePerm)
+	os.Mkdir("temp", os.ModePerm)
+
+	// TODO: Create a deep directory
+	os.MkdirAll("misc/temp", os.ModePerm)
+
+	// TODO: Remove will remove an item
+	os.Remove("utils")
+
+	// TODO: RemoveAll will remove an item and all children
+	os.RemoveAll("misc")
+
+	// TODO: Get the current working directory
+	dir, _ := os.Getwd()
+	fmt.Println("Current working directory:", dir)
+
+	// TODO: Get the directory of the current process
+	exedir, _ := os.Executable()
+	fmt.Println("Path to exe:", exedir)
+
+	// TODO: Read the contents of a directory
+	constents, _ := os.ReadDir(".")
+	for _, file := range constents {
+		fmt.Println(file.Name(), file.IsDir())
+	}
 }
