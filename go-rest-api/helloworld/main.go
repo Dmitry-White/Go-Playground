67f6f6c6782384816e14c7ed5ac528f83c837b8c
Dmitry White <dmitry.b.white@gmail.com>
2023-02-25 14:44:15 +0000

    Add Go Rest API helloworld implementation

---
+++ go-rest-api/helloworld/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const PORT = ":8000"
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
+
+func main() {
+	http.HandleFunc("/", helloWorld)
+
+	fmt.Printf("Server started and listening on localhost%s", PORT)
+
+	err := http.ListenAndServe(PORT, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
