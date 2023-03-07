25da95451bdf5599b667ada3c740f8842060693e
Dmitry White <dmitry.b.white@gmail.com>
2023-03-07 22:37:57 +0000

    Add Go Rest API test non existent product

--- go-rest-api/api/tests/api_test.go
+++ go-rest-api/api/tests/api_test.go
@@ -1,7 +1,9 @@
 package tests
 
 import (
+	"encoding/json"
 	"go-rest-api/api"
+	"net/http"
 	"os"
 	"testing"
 )
@@ -16,4 +18,22 @@ func TestMain(m *testing.M) {
 
 	clearProductTable(&app)
 	os.Exit(code)
+}
+
+func TestGetNonExistentProduct(t *testing.T) {
+	clearProductTable(&app)
+
+	request, _ := http.NewRequest("GET", "/product/13", nil)
+	response := executeRequest(&app, request)
+
+	checkResponseCode(t, http.StatusInternalServerError, response.Code)
+
+	m := map[string]string{}
+	json.Unmarshal(response.Body.Bytes(), &m)
+	if m["error"] != "sql: no rows in result set" {
+		t.Errorf(
+			"Expected the 'error' key of the response to be set to 'sql: no rows in result set'. Got '%s'",
+			m["error"],
+		)
+	}
 }
