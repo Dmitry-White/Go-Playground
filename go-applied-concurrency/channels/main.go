23f7bdd95f11d8784f14e832c365929f9647011c
Dmitry White <dmitry.b.white@gmail.com>
2023-03-12 22:45:39 +0000

    Add Go Applied Concurrency channels buffered implementation

--- go-applied-concurrency/channels/main.go
+++ go-applied-concurrency/channels/main.go
@@ -6,4 +6,8 @@ func main() {
 	fmt.Println("----------- Unbuffered -----------")
 	unbuffered()
 	fmt.Println("----------------------------------")
+
+	fmt.Println("------------ Buffered ------------")
+	buffered()
+	fmt.Println("----------------------------------")
 }
