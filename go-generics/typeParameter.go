8047761c02b59b2536d6587bbac8d3b01016e861
Dmitry White <dmitry.b.white@gmail.com>
2023-03-18 21:35:39 +0000

    Add Go Generics type parameter generic function

--- go-generics/typeParameter.go
+++ go-generics/typeParameter.go
@@ -14,19 +14,23 @@ type Wind struct {
 	Netto float64
 }
 
+var kinetecoPrint string = "Kineteco Deal:"
+
 // Print prints the information for a solar product.
 // The string is enriched with the required kineteco legal information.
-func (s *Solar) Print() string {
+func (s *Solar) print() string {
 	return fmt.Sprintf("%s - %v\n", kinetecoPrint, *s)
 }
 
 // Print prints the information for a wind product.
 // The string is enriched with the required kineteco legal information.
-func (w *Wind) Print() string {
+func (w *Wind) print() string {
 	return fmt.Sprintf("%s - %v\n", kinetecoPrint, *w)
 }
 
-var kinetecoPrint string = "Kineteco Deal:"
+func PrintGeneric[T any](s T) string {
+	return fmt.Sprintf("%s - %v\n", kinetecoPrint, s)
+}
 
 func typeParameter() {
 	// Create three different energy offers of kineteco
@@ -35,7 +39,8 @@ func typeParameter() {
 	windwest := Wind{Name: "Wind West", Netto: 3.950}
 
 	// Print details for each energy offer with kineteco branding
-	fmt.Println(solar2k.Print())
-	fmt.Println(solar3k.Print())
-	fmt.Println(windwest.Print())
+	fmt.Println(solar2k.print())
+	fmt.Println(solar3k.print())
+	fmt.Println(windwest.print())
+	fmt.Println(PrintGeneric[Solar](solar2k))
 }
