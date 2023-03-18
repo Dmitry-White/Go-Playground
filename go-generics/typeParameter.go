895f6abcaeefc40bd8b895ef05340c5bd21de93e
Dmitry White <dmitry.b.white@gmail.com>
2023-03-18 21:45:06 +0000

    Add Go Generics type parameter generic slice

--- go-generics/typeParameter.go
+++ go-generics/typeParameter.go
@@ -28,8 +28,14 @@ func (w *Wind) print() string {
 	return fmt.Sprintf("%s - %v\n", kinetecoPrint, *w)
 }
 
-func PrintGeneric[T any](s T) string {
+func printGeneric[T any](s T) string {
 	return fmt.Sprintf("%s - %v\n", kinetecoPrint, s)
+}
+
+func printSlice[T any](sl []T) {
+	for i, el := range sl {
+		fmt.Printf("%d: %s", i, printGeneric[T](el))
+	}
 }
 
 func typeParameter() {
@@ -42,5 +48,10 @@ func typeParameter() {
 	fmt.Println(solar2k.print())
 	fmt.Println(solar3k.print())
 	fmt.Println(windwest.print())
-	fmt.Println(PrintGeneric[Solar](solar2k))
+	fmt.Println(printGeneric[Solar](solar2k))
+
+	solarSlice := []Solar{solar2k, solar3k}
+	windSlice := []Wind{windwest, windwest}
+	printSlice[Solar](solarSlice)
+	printSlice[Wind](windSlice)
 }
