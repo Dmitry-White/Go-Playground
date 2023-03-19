9c896e61601f8ee185998f08b49c6da629396b4a
Dmitry White <dmitry.b.white@gmail.com>
2023-03-19 17:46:28 +0000

    Add Go Generics type set module setup

--- go-generics/main.go
+++ go-generics/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"go-generics/parameter"
+	"go-generics/set"
 )
 
 func main() {
 	fmt.Println("----------- Type Parameter -----------")
 	parameter.TypeParameter()
+	fmt.Println("--------------------------------------")
+
+	fmt.Println("-------------- Type Set --------------")
+	set.TypeSet()
 	fmt.Println("--------------------------------------")
 }
---
+++ go-generics/set/types.go
@@ -0,0 +1,13 @@
+package set
+
+// Solar handles all the different energy offers powered by solar.
+type Solar struct {
+	Name  string
+	Netto float64
+}
+
+// Wind handles all the different energy offers powered by wind.
+type Wind struct {
+	Name  string
+	Netto float64
+}
