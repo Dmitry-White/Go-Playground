6575f0a9d02be49d60dece2fb5877f1987728aea
Dmitry White <dmitry.b.white@gmail.com>
2023-03-24 22:25:13 +0000

    Add Go Security Size limit safe implementation

--- go-security/input/size-limit/server.go
+++ go-security/input/size-limit/server.go
@@ -1,16 +1,47 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
 
+func getSafeReader(req *http.Request) (io.Reader, error) {
+	return io.LimitReader(req.Body, 1), nil
+}
+
+func getHackyReader(req *http.Request) (io.ReadCloser, error) {
+	return req.Body, nil
+}
+
+func getReader(strategy string) func(req *http.Request) (io.Reader, error) {
+	choice := fmt.Sprintf("Using %s reader", strategy)
+	log.Println(choice)
+
+	return func(req *http.Request) (io.Reader, error) {
+		switch strategy {
+		case "safe":
+			return getSafeReader(req)
+		case "hacky":
+			return getHackyReader(req)
+		}
+		return nil, errors.New("this strategy is not supported")
+	}
+}
+
 func handler(res http.ResponseWriter, req *http.Request) {
 	defer req.Body.Close()
 
-	data, err := io.ReadAll(req.Body)
+	reader, err := getReader("hacky")(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(reader)
+
+	data, err := io.ReadAll(reader)
 	if err != nil {
 		log.Fatalln(err)
 	}
