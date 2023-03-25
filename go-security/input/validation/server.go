16abff7249bcd31d935b4863d7e4eb32a2872efd
Dmitry White <dmitry.b.white@gmail.com>
2023-03-25 20:15:04 +0000

    Add Go Security Validation basic handler

--- go-security/input/validation/server.go
+++ go-security/input/validation/server.go
@@ -1,10 +1,33 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
 
+type Payment struct {
+	Time   string
+	User   string
+	To     string
+	Amount int
+}
+
 func handler(res http.ResponseWriter, req *http.Request) {
-	log.Println("Placeholder")
+	defer req.Body.Close()
+
+	decoder := json.NewDecoder(req.Body)
+
+	var p Payment
+	err := decoder.Decode(&p)
+	if err != nil {
+		log.Print(err)
+		http.Error(res, "Bad JSON", http.StatusBadRequest)
+		return
+	}
+
+	log.Println("Payment:", p)
+
+	fmt.Fprintln(res, p)
 }
