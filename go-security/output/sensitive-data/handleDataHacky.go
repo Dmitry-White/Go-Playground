3ab7e8b4e48885940fed09ece0e7f26aaeee9905
Dmitry White <dmitry.b.white@gmail.com>
2023-03-28 23:30:49 +0100

    Add Go Security Sensitive data handler strategies implementation

---
+++ go-security/output/sensitive-data/handleDataHacky.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"go-security/output/sensitive-data/dal"
+	"net/http"
+)
+
+func handleDataHacky(resw http.ResponseWriter, user *dal.User) {
+	friends, err := dal.GetUserFriends(user)
+	if err != nil {
+		http.Error(resw, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	resw.Header().Set("Content-Type", "application/json")
+
+	jsonWriter := json.NewEncoder(resw)
+	jsonWriter.Encode(friends)
+}
---
+++ go-security/output/sensitive-data/handleDataSafe.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"go-security/output/sensitive-data/dal"
+	"log"
+	"net/http"
+)
+
+func handleDataSafe(resw http.ResponseWriter, user *dal.User) {
+	friends, err := dal.GetUserFriends(user)
+	if err != nil {
+		log.Println("Error: ", err)
+		http.Error(resw, "Can't get friends", http.StatusBadRequest)
+		return
+	}
+
+	resw.Header().Set("Content-Type", "application/json")
+
+	jsonWriter := json.NewEncoder(resw)
+	jsonWriter.Encode(friends)
+}
