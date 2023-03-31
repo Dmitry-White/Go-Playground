363403ef11f89d1472b9a33dfd5a8d589649661a
Dmitry White <dmitry.b.white@gmail.com>
2023-03-31 23:41:33 +0100

    Add Go Security Infrastructure SSL certificates server implementation

--- go-security/infrastructure/ssl-certificates/main.go
+++ go-security/infrastructure/ssl-certificates/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"net/http"
+)
+
+const (
+	certFile = "cert.pem"
+	keyFile  = "key.pem"
+)
+
+func healthHandler(resw http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(resw, "OK\n")
+}
 
 func main() {
-	log.Println("Not Implemented")
+	router := http.ServeMux{}
+
+	router.HandleFunc("/health", healthHandler)
+
+	http.ListenAndServeTLS(":8080", certFile, keyFile, &router)
 }
