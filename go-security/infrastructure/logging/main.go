a323206ba6c39a6a0875b834635c66c96b8c3f00
Dmitry White <dmitry.b.white@gmail.com>
2023-04-04 18:32:05 +0100

    Add Go Security Infrastructure Logging metrics setup

--- go-security/infrastructure/logging/main.go
+++ go-security/infrastructure/logging/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"expvar"
 	"log"
 	"net/http"
 )
+
+/*
+	`expvar` is the native language library if we want to instrument or add metrics to a Go app.
+	When we import the expvar library, it's `init` registers http.Handler and a couple of variables.
+	This registers `/debug/vars` at default http ServeMux as an endpoint
+	where all the metrics will be published in JSON format.
+	Using your own ServeMux requires explicit `/debug/vars` registering.
+*/
 
 func main() {
 	router := http.ServeMux{}
 
 	router.HandleFunc("/login", loginHandler)
+	router.Handle("/debug/vars", expvar.Handler())
 
 	err := http.ListenAndServe(":8080", &router)
 	if err != nil {
--- go-security/infrastructure/logging/server.go
+++ go-security/infrastructure/logging/server.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"expvar"
 	"log"
 	"net/http"
 )
 
+var loginCalls = expvar.NewInt("login.calls")
+
 func loginHandler(resw http.ResponseWriter, req *http.Request) {
 	log.Println("Not Implemented")
+	loginCalls.Add(1)
 }
