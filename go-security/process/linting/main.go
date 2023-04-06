579e5332af0dadc29bf3651061216e3c3f35d01b
Dmitry White <dmitry.b.white@gmail.com>
2023-04-06 21:38:11 +0100

    Add Go Security Process Linting gofmt instructions

--- go-security/process/linting/main.go
+++ go-security/process/linting/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 )
@@ -24,7 +24,7 @@ func main() {
 	}
 
 	defer resp.Body.Close()
-	data, err := ioutil.ReadAll(resp.Body)
+	data, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
