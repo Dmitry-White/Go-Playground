ab1387c7ccbb37415589ff50eee68d4c1eea36fc
Dmitry White <dmitry.b.white@gmail.com>
2023-04-09 21:48:20 +0100

    Add Go Recipes Basics slices setup

--- go-recipes/basics/main.go
+++ go-recipes/basics/main.go
@@ -5,11 +5,16 @@ import (
 )
 
 func main() {
-	fmt.Println("----------- Mean -----------")
+	fmt.Println("-------------- Mean --------------")
 	fmt.Println(meanNaive([]int{1, 2, 3}))    // 2
 	fmt.Println(meanNaive([]int{1, 2, 3, 4})) // 2
 
 	fmt.Println(meanSafe([]int{1, 2, 3}))    // 2
 	fmt.Println(meanSafe([]int{1, 2, 3, 4})) // 2.5
+	fmt.Println("----------------------------------")
+
+	fmt.Println("------------- Median -------------")
+	fmt.Println(median([]float64{3, 1, 2}))    // 2
+	fmt.Println(median([]float64{3, 1, 4, 2})) // 2.5
 	fmt.Println("----------------------------------")
 }
---
+++ go-recipes/basics/slices.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func median(nums []float64) float64 {
+	fmt.Println("Not Implemented")
+	return 0.0
+}
