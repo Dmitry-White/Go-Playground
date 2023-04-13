653f44366efe7c3c8ac9a113bdc73023ad70ff74
Dmitry White <dmitry.b.white@gmail.com>
2023-04-13 17:40:23 +0100

    Add Go Recipes Basics close implementation

--- go-recipes/basics/close.go
+++ go-recipes/basics/close.go
@@ -1,8 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Item struct {
+	SKU  string
+	Name string
+}
+
+func writeItems(filename string, items []Item) error {
+	fmt.Println("Not Implemented")
+	fmt.Printf("Input %v into %s\n", items, filename)
+	return nil
+}
 
 func close(filename string) error {
-	fmt.Println("Not Implemented")
+	items := []Item{
+		{"m183x", "Magic Wand"},
+		{"m184y", "Invisibility Cape"},
+		{"m185z", "Levitation Spell"},
+	}
+
+	err := writeItems(filename, items)
+	if err != nil {
+		return err
+	}
 	return nil
 }
