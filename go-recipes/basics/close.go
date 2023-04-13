c3b0b42eec0108fb4907789282fdc3122b5b0eaa
Dmitry White <dmitry.b.white@gmail.com>
2023-04-13 17:46:52 +0100

    Add Go Recipes Basics writeItems implementation

--- go-recipes/basics/close.go
+++ go-recipes/basics/close.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"encoding/csv"
+	"os"
 )
 
 type Item struct {
@@ -10,9 +11,32 @@ type Item struct {
 }
 
 func writeItems(filename string, items []Item) error {
-	fmt.Println("Not Implemented")
-	fmt.Printf("Input %v into %s\n", items, filename)
-	return nil
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	header := []string{"sku", "name"}
+
+	csvWriter := csv.NewWriter(file)
+	defer csvWriter.Flush()
+
+	err = csvWriter.Write(header)
+	if err != nil {
+		return err
+	}
+
+	for _, item := range items {
+		row := []string{item.SKU, item.Name}
+
+		err := csvWriter.Write(row)
+		if err != nil {
+			return err
+		}
+	}
+
+	return csvWriter.Error()
 }
 
 func close(filename string) error {
