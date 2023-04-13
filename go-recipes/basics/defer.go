4ad5353b904c4034e9a1b55408c02d0403724bd7
Dmitry White <dmitry.b.white@gmail.com>
2023-04-13 18:01:31 +0100

    Add Go Recipes Basics defer withRecovery implementation

--- go-recipes/basics/defer.go
+++ go-recipes/basics/defer.go
@@ -1,11 +1,28 @@
 package main
 
-func doDefer() error {
-	nums := []int{1}
-	panickingHandler(nums)
-	return nil
-}
+import "fmt"
 
 func panickingHandler(nums []int) int {
 	return nums[len(nums)-2]
+}
+
+func withRecovery(handler func(args []int) int, args []int) (_ interface{}, err error) {
+	defer func() {
+		if e := recover(); e != nil { // e is interface{}
+			err = fmt.Errorf("%v", e)
+		}
+	}()
+
+	return handler(args), nil
+}
+
+func doDefer() error {
+	nums := []int{1}
+
+	// panickingHandler(nums)
+	_, err := withRecovery(panickingHandler, nums)
+	if err != nil {
+		return err
+	}
+	return nil
 }
