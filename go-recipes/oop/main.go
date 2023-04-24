9a7841a67c826bb863f91309bb0cc138f8ebd2d4
Dmitry White <dmitry.b.white@gmail.com>
2023-04-24 23:36:10 +0100

    Add Go Recipes OOP methods setup

--- go-recipes/oop/main.go
+++ go-recipes/oop/main.go
@@ -10,4 +10,8 @@ func main() {
 	fmt.Printf("%+v\n", getDoorEvent("front door", time.Now(), "open"))
 	fmt.Printf("%+v\n", getTermostatEvent("front door", time.Now(), 25))
 	fmt.Println("----------------------------------")
+
+	fmt.Println("------------- Methods ------------")
+	fmt.Println(getTermostat())
+	fmt.Println("----------------------------------")
 }
---
+++ go-recipes/oop/methods.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func getTermostat() error {
+	fmt.Println("Not Implemented")
+	return nil
+}
