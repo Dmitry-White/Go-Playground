2885f001f1c86a4c852c86f5f3a1980d597decd5
Dmitry White <dmitry.b.white@gmail.com>
2023-04-25 11:41:17 +0100

    Add Go Recipes OOP empty interfaces implementation

--- go-recipes/oop/empty.go
+++ go-recipes/oop/empty.go
@@ -1,8 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+)
 
-func getCounts() error {
-	fmt.Println("Not Implemented")
-	return nil
+type ClickEvent struct{}
+type HoverEvent struct{}
+
+var eventsCounter = make(map[string]int)
+
+func record(event interface{}) {
+	switch event.(type) {
+	case *ClickEvent:
+		eventsCounter["click"]++
+	case *HoverEvent:
+		eventsCounter["hover"]++
+	default:
+		log.Printf("warning: unknown event: %#v of type %T\n", event, event)
+	}
+}
+
+func getCounts() map[string]int {
+	record(&ClickEvent{})
+	record(&HoverEvent{})
+	record(&ClickEvent{})
+	record(3)
+	return eventsCounter
 }
