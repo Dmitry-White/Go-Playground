9652dcfdb8f6ea9329cff8edb669cbf62c67418b
Dmitry White <dmitry.b.white@gmail.com>
2023-04-25 12:28:01 +0100

    Add Go Recipes OOP iota log levels setup

--- go-recipes/oop/iota.go
+++ go-recipes/oop/iota.go
@@ -2,7 +2,24 @@ package main
 
 import "fmt"
 
-// https://github.com/golang/go/wiki/Iota
+type LogLevel uint8
+
+/*
+In some programming languages (C++, Go, etc.),
+iota is used to represent and generate
+an array of consecutive integers.
+
+https://en.wikipedia.org/wiki/Iota
+
+https://github.com/golang/go/wiki/Iota
+*/
+const (
+	Debug LogLevel = iota + 1
+	Warning
+	Info
+	Error
+)
+
 func initLogLevel() error {
 	fmt.Println("Not Implemented")
 	return nil
