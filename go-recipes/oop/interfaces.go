2feed8bcbc04fcdb0a73d7add6af3b1a73ae0110
Dmitry White <dmitry.b.white@gmail.com>
2023-04-25 11:03:31 +0100

    Add Go Recipes OOP interfaces sernsor implementation

--- go-recipes/oop/interfaces.go
+++ go-recipes/oop/interfaces.go
@@ -2,7 +2,18 @@ package main
 
 import "fmt"
 
-func getSensors() error {
+type Sensor interface {
+	id() string
+	kind() string
+}
+
+func printSensors(sensors []Sensor) {
+	for _, sensor := range sensors {
+		fmt.Printf("%s <%s>\n", sensor.id(), sensor.kind())
+	}
+}
+
+func getSensors() Sensor {
 	fmt.Println("Not Implemented")
 	return nil
 }
