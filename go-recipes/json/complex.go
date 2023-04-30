76d65073c9700f10850fa374e9208f85ee37adc7
Dmitry White <dmitry.b.white@gmail.com>
2023-04-30 22:20:56 +0100

    Add Go Recipes JSON stations parsing

--- go-recipes/json/complex.go
+++ go-recipes/json/complex.go
@@ -1,18 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io"
 	"os"
+	"time"
 )
 
-func scanStations() error {
+type Station struct {
+	Name      string
+	Status    string
+	LastCheck struct {
+		Time string
+	}
+}
+
+type Reply struct {
+	LastCheckTime string
+	Stations      []Station
+}
+
+func getLaggingStations(reader io.Reader, lag time.Duration) (interface{}, error) {
+	var reply Reply
+
+	decoder := json.NewDecoder(reader)
+	err := decoder.Decode(&reply)
+	if err != nil {
+		return nil, err
+	}
+
+	return reply, nil
+}
+
+func scanStations() interface{} {
 	file, err := os.Open("stations.json")
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
-	fmt.Println("File: ", file)
+	lagging, err := getLaggingStations(file, time.Minute)
+	if err != nil {
+		return err
+	}
 
-	return nil
+	return lagging
 }
