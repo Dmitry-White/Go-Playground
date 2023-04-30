e2614aca7cfbb8c10885c52dac04587807360d1f
Dmitry White <dmitry.b.white@gmail.com>
2023-04-30 22:24:37 +0100

    Add Go Recipes JSON stations scanning

--- go-recipes/json/complex.go
+++ go-recipes/json/complex.go
@@ -20,8 +20,13 @@ type Reply struct {
 	Stations      []Station
 }
 
-func getLaggingStations(reader io.Reader, lag time.Duration) (interface{}, error) {
-	var reply Reply
+func parseTime(ts string) (time.Time, error) {
+	return time.Parse("2006-01-02 15:04:05 PM", ts)
+}
+
+func getLaggingStations(reader io.Reader, lag time.Duration) ([]string, error) {
+
+	reply := Reply{}
 
 	decoder := json.NewDecoder(reader)
 	err := decoder.Decode(&reply)
@@ -29,7 +34,28 @@ func getLaggingStations(reader io.Reader, lag time.Duration) (interface{}, error
 		return nil, err
 	}
 
-	return reply, nil
+	checkTime, err := parseTime(reply.LastCheckTime)
+	if err != nil {
+		return nil, err
+	}
+
+	lagging := []string{}
+	for _, station := range reply.Stations {
+		if station.Status != "Active" {
+			continue
+		}
+
+		lastCheck, err := parseTime(station.LastCheck.Time)
+		if err != nil {
+			return nil, err
+		}
+
+		if checkTime.Sub(lastCheck) > lag {
+			lagging = append(lagging, station.Name)
+		}
+	}
+
+	return lagging, nil
 }
 
 func scanStations() interface{} {
