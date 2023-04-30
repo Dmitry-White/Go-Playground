5845185e654ddd53b0468b0b7fd85952a42a19a5
Dmitry White <dmitry.b.white@gmail.com>
2023-04-30 22:10:14 +0100

    Add Go Recipes JSON complex setup

--- go-recipes/json/main.go
+++ go-recipes/json/main.go
@@ -6,4 +6,8 @@ func main() {
 	fmt.Println("------------ Unmarshal -----------")
 	fmt.Printf("%+v\n", getTemperature())
 	fmt.Println("----------------------------------")
+
+	fmt.Println("------------- Complex ------------")
+	fmt.Printf("%+v\n", scanStations())
+	fmt.Println("----------------------------------")
 }
