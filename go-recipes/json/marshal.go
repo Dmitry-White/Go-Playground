2e5380ed74f2aa33753bc0afee2e9e22226a3bd2
Dmitry White <dmitry.b.white@gmail.com>
2023-05-01 18:26:58 +0100

    Add Go Recipes JSON marshal naive

--- go-recipes/json/marshal.go
+++ go-recipes/json/marshal.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"os"
+)
 
-func getQuantity() error {
-	fmt.Println("Not Implemented")
-	return nil
+// Quantity is combination of value and unit (e.g. 2.7cm)
+type Quantity struct {
+	Value float64
+	Unit  string
+}
+
+func getQuantity() interface{} {
+	q := Quantity{1.78, "meter"}
+
+	encoder := json.NewEncoder(os.Stdout)
+	err := encoder.Encode(&q)
+	if err != nil {
+		return err
+	}
+
+	return &q
 }
