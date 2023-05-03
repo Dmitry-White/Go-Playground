586f76d769053b51d2d40248041eabfaede7ccfb
Dmitry White <dmitry.b.white@gmail.com>
2023-05-03 17:27:30 +0100

    Add Go Recipes JSON unknown requests

--- go-recipes/json/unknown.go
+++ go-recipes/json/unknown.go
@@ -1,8 +1,34 @@
 package main
 
-import "fmt"
+// StartJob is a request to start a job
+type StartJob struct {
+	Type  string
+	User  string
+	Count int
+}
 
-func handleRequests() error {
-	fmt.Println("Not Implemented")
-	return nil
+// JobStatus is a request for job status
+type JobStatus struct {
+	Type string
+	ID   string
+}
+
+func handleRequest(data []byte) (interface{}, error) {
+	return data, nil
+}
+
+func handleRequests() interface{} {
+	firstRequest := []byte(`{"type": "start", "user": "joe", "count": 7}`)
+	firstResponse, err := handleRequest(firstRequest)
+	if err != nil {
+		return err
+	}
+
+	secondRequest := []byte(`{"type": "status", "id": "seven"}`)
+	secondResponse, err := handleRequest(secondRequest)
+	if err != nil {
+		return err
+	}
+
+	return []interface{}{firstResponse, secondResponse}
 }
