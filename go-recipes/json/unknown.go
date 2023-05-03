44b669aafee76a0425ee61ecd3115044e1002a3a
Dmitry White <dmitry.b.white@gmail.com>
2023-05-03 17:45:46 +0100

    Add Go Recipes JSON unknown mapstructure usage

--- go-recipes/json/unknown.go
+++ go-recipes/json/unknown.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/mitchellh/mapstructure"
+)
+
 // StartJob is a request to start a job
 type StartJob struct {
 	Type  string
@@ -13,8 +20,53 @@ type JobStatus struct {
 	ID   string
 }
 
+func handleStartJob(raw map[string]interface{}) StartJob {
+	startJob := StartJob{}
+
+	err := mapstructure.Decode(raw, &startJob)
+	if err != nil {
+		fmt.Println("Can't decode raw data", raw)
+	}
+
+	return startJob
+}
+
+func handleJobStatus(raw map[string]interface{}) JobStatus {
+	jobStatus := JobStatus{}
+
+	err := mapstructure.Decode(raw, &jobStatus)
+	if err != nil {
+		fmt.Println("Can't decode raw data", raw)
+	}
+
+	return jobStatus
+}
+
 func handleRequest(data []byte) (interface{}, error) {
-	return data, nil
+	temp := map[string]interface{}{}
+	err := json.Unmarshal(data, &temp)
+	if err != nil {
+		return nil, err
+	}
+
+	field, ok := temp["type"]
+	if !ok {
+		return nil, fmt.Errorf("no type field")
+	}
+
+	typeField, ok := field.(string)
+	if !ok {
+		return nil, fmt.Errorf("type is not a string")
+	}
+
+	switch typeField {
+	case "start":
+		return handleStartJob(temp), nil
+	case "status":
+		return handleJobStatus(temp), nil
+	}
+
+	return nil, fmt.Errorf("unknown request type")
 }
 
 func handleRequests() interface{} {
