65a3e1893424d27629648eb59e5bc09d1a8ae633
Dmitry White <dmitry.b.white@gmail.com>
2023-05-04 17:08:57 +0100

    Add Go Recipes HTTP client posting

--- go-recipes/http/client.go
+++ go-recipes/http/client.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"time"
 )
 
@@ -12,9 +17,60 @@ type Metric struct {
 	Memory float64   `json:"memory"` // MB
 }
 
-func postMetric(m *Metric) (*Metric, error) {
-	fmt.Println(m)
-	return nil, nil
+type MetricResponse struct {
+	Json Metric
+}
+
+const (
+	BASE_URL = "https://httpbin.org"
+	ENDPOINT = "post"
+	TIMEOUT  = 5 * time.Second
+	SIZE     = 1 << 20
+)
+
+func postMetric(m Metric) (*MetricResponse, error) {
+	fmt.Println("Metric:", m)
+
+	data, err := json.Marshal(m)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println("Bytes:", data)
+
+	url := fmt.Sprintf("%s/%s", BASE_URL, ENDPOINT)
+	body := bytes.NewReader(data)
+
+	ctx, cancel := context.WithTimeout(context.Background(), TIMEOUT)
+	defer cancel()
+
+	request, err := http.NewRequestWithContext(ctx, http.MethodPost, url, body)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Set("Content-Type", "application/json")
+	fmt.Println("Request:", request)
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("bad status: %d %s", response.StatusCode, response.Status)
+	}
+	defer response.Body.Close()
+	fmt.Println("Response:", response)
+
+	result := MetricResponse{}
+	reader := io.LimitReader(response.Body, SIZE)
+	decoder := json.NewDecoder(reader)
+
+	decoderErr := decoder.Decode(&result)
+	if err != nil {
+		return nil, decoderErr
+	}
+	fmt.Printf("Result: %#v\n", result)
+
+	return &result, nil
 }
 
 func sendMetrics() interface{} {
@@ -24,7 +80,7 @@ func sendMetrics() interface{} {
 		Memory: 87.32,
 	}
 
-	reply, err := postMetric(&m)
+	reply, err := postMetric(m)
 	if err != nil {
 		return err
 	}
