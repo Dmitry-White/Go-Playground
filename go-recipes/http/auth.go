3b53283632d0755ca5acc92c71a898a23ce5434f
Dmitry White <dmitry.b.white@gmail.com>
2023-05-04 21:16:55 +0100

    Add Go Recipes HTTP auth setup

---
+++ go-recipes/http/auth.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func authenticate() error {
+	fmt.Println("Not Implemented")
+	return nil
+}
--- go-recipes/http/main.go
+++ go-recipes/http/main.go
@@ -6,4 +6,8 @@ func main() {
 	fmt.Println("------------- Client -------------")
 	fmt.Printf("%+v\n", sendMetrics())
 	fmt.Println("----------------------------------")
+
+	fmt.Println("-------------- Auth --------------")
+	fmt.Printf("%+v\n", authenticate())
+	fmt.Println("----------------------------------")
 }
