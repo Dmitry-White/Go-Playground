aa7a3950c6bdd36c7a96969014e0c678d724d213
Dmitry White <dmitry.b.white@gmail.com>
2023-05-04 17:03:57 +0100

    Add Go Recipes HTTP client metrics

--- go-recipes/http/client.go
+++ go-recipes/http/client.go
@@ -1,8 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func sendMetrics() error {
-	fmt.Println("Not Implemented")
-	return nil
+// Metric is an application metric
+type Metric struct {
+	Time   time.Time `json:"time"`
+	CPU    float64   `json:"cpu"`    // CPU load
+	Memory float64   `json:"memory"` // MB
+}
+
+func postMetric(m *Metric) (*Metric, error) {
+	fmt.Println(m)
+	return nil, nil
+}
+
+func sendMetrics() interface{} {
+	m := Metric{
+		Time:   time.Now(),
+		CPU:    0.23,
+		Memory: 87.32,
+	}
+
+	reply, err := postMetric(&m)
+	if err != nil {
+		return err
+	}
+
+	return reply
 }
