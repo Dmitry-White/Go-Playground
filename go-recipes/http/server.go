a334088bd6632a7a8a8f0ef0f102860c2839d86d
Dmitry White <dmitry.b.white@gmail.com>
2023-05-06 17:11:07 +0100

    Add Go Recipes Server config setup

--- go-recipes/http/server.go
+++ go-recipes/http/server.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
-func startServer() error {
-	fmt.Println("Not Implemented")
-	return nil
+func handleMetric(resw http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(resw, "OK\n")
+}
+
+func startServer() interface{} {
+	router := http.ServeMux{}
+
+	router.HandleFunc("/metric", handleMetric)
+
+	err := http.ListenAndServe(":8000", &router)
+	if err != nil {
+		return nil
+	}
+	return &router
 }
