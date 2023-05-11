d30e4ccdb65e72f3c43398eb4c0e0a4aa10089c3
Dmitry White <dmitry.b.white@gmail.com>
2023-05-11 17:52:29 +0100

    Add Go Recipes Concurrency errors setup

--- go-recipes/concurrency/errors.go
+++ go-recipes/concurrency/errors.go
@@ -1,10 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func getErrors() error {
-	fmt.Println("Not Implemented")
-	// TODO:
-	// https://www.fullstory.com/blog/why-errgroup-withcontext-in-golang-server-handlers/
-	return nil
+/*
+	More Info: https://www.fullstory.com/blog/why-errgroup-withcontext-in-golang-server-handlers/
+*/
+
+const (
+	URL_TEMPLATE = "https://d37ci6vzurychx.cloudfront.net/trip-data/%s_tripdata_2020-%02d.parquet"
+	START_MONTH  = 1
+	END_MONTH    = 12
+)
+
+func getErrors() interface{} {
+	urls := generateURLs()
+
+	size, err := analyzeURLs(urls)
+	if err != nil {
+		return err
+	}
+
+	sizeGB := float64(size) / (1 << 30)
+	result := fmt.Sprintf("size = %.2fGB\n", sizeGB)
+
+	return result
 }
--- go-recipes/concurrency/go.mod
+++ go-recipes/concurrency/go.mod
@@ -1,3 +1,5 @@
 module go-recipes/concurrency
 
 go 1.20
+
+require golang.org/x/sync v0.2.0 // indirect
