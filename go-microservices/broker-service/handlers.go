0e447e96575149575b8973ab5178283bfc4624ac
Dmitry White <dmitry.b.white@gmail.com>
2023-07-21 20:37:29 +0100

    Add Go Microservices broker process handler

--- go-microservices/broker-service/handlers.go
+++ go-microservices/broker-service/handlers.go
@@ -1,11 +1,50 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
 )
 
-func (a *AppConfig) handleIndex(resw http.ResponseWriter, req *http.Request) {
+func (app *AppConfig) handleIndex(resw http.ResponseWriter, req *http.Request) {
 	data := generateJson()
 
 	writeJSON(resw, http.StatusAccepted, data)
+}
+
+func processAuth(resw http.ResponseWriter, requestPayload *RequestPayload) {
+	data, err := authenticate(requestPayload.Auth)
+	if err != nil {
+		errorJSON(resw, err)
+		return
+	}
+	if data.Error {
+		errorJSON(resw, errors.New(data.Message), http.StatusUnauthorized)
+		return
+	}
+
+	responsePayload := jsonResponse{
+		Error:   false,
+		Message: fmt.Sprintf("Processed Auth request: %s", data.Message),
+		Data:    data,
+	}
+	writeJSON(resw, http.StatusAccepted, responsePayload)
+}
+
+func (app *AppConfig) handleProcess(resw http.ResponseWriter, req *http.Request) {
+	var requestPayload RequestPayload
+
+	err := readJSON(resw, req, &requestPayload)
+	if err != nil {
+		errorJSON(resw, err)
+		return
+	}
+
+	switch requestPayload.Action {
+	case SERVICES.Auth.Name:
+		processAuth(resw, &requestPayload)
+	default:
+		errorJSON(resw, errors.New("unknown Action"))
+		return
+	}
 }
--- go-microservices/broker-service/services.go
+++ go-microservices/broker-service/services.go
@@ -1,9 +1,60 @@
 package main
 
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+)
+
 func generateJson() jsonResponse {
 	payload := jsonResponse{
 		Error:   false,
 		Message: "Hit the broker",
 	}
 	return payload
+}
+
+func authenticate(payload AuthPayload) (*jsonResponse, error) {
+	authRequest, err := json.MarshalIndent(payload, "", "\t")
+	if err != nil {
+		log.Println("[authenticate] Marshal Error: ", err)
+		return nil, err
+	}
+
+	authRequestReader := bytes.NewBuffer(authRequest)
+	request, err := http.NewRequest(http.MethodPost, SERVICES.Auth.URL, authRequestReader)
+	if err != nil {
+		log.Println("[authenticate] Request Error: ", err)
+		return nil, err
+	}
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		log.Println("[authenticate] Client Error: ", err)
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode == http.StatusBadRequest || response.StatusCode == http.StatusUnauthorized {
+		responseErr := errors.New("invalid credentials")
+		return nil, responseErr
+	}
+
+	if response.StatusCode != http.StatusAccepted {
+		responseErr := errors.New("error calling auth service")
+		return nil, responseErr
+	}
+
+	var jsonFromService jsonResponse
+	decoder := json.NewDecoder(response.Body)
+	decodeErr := decoder.Decode(&jsonFromService)
+	if decodeErr != nil {
+		log.Println("[authenticate] Decode Error: ", decodeErr)
+		return nil, decodeErr
+	}
+
+	return &jsonFromService, nil
 }
