8b36e6ec4f377076a2825ccf7199bab185240dd1
Dmitry White <dmitry.b.white@gmail.com>
2023-07-28 20:04:52 +0100

    Add Go Microservices frontend service logger request

--- go-microservices/frontend-service/web/js/broker.js
+++ go-microservices/frontend-service/web/js/broker.js
@@ -1,5 +1,6 @@
 const brokerBtn = document.getElementById("brokerBtn");
 const authBrokerBtn = document.getElementById("authBrokerBtn");
+const logBrokerBtn = document.getElementById("logBrokerBtn");
 
 brokerBtn.addEventListener("click", async () => {
   const { BASE, INDEX } = API.BROKER;
@@ -33,6 +34,44 @@ authBrokerBtn.addEventListener("click", async () => {
     auth: {
       email: "admin@example.com",
       password: "verysecret",
+    },
+  };
+
+  const headers = new Headers({
+    "Content-Type": "application/json",
+  });
+
+  const body = {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(payload),
+  };
+  const URL = `${BASE}${PROCESS}`;
+
+  try {
+    const response = await fetch(URL, body);
+    const data = await response.json();
+
+    sentBox.textContent = JSON.stringify(payload, undefined, 4);
+    receivedBox.textContent = JSON.stringify(data, undefined, 4);
+
+    if (data.error) {
+      throw new Error(data.message);
+    }
+    outputBox.innerHTML += `<br><strong>Response from broker service</strong>: ${data.message}`;
+  } catch (error) {
+    outputBox.innerHTML += "<br><strong>Error</strong>:" + error;
+  }
+});
+
+logBrokerBtn.addEventListener("click", async () => {
+  const { BASE, PROCESS } = API.BROKER;
+
+  const payload = {
+    action: "Log",
+    log: {
+      name: "event",
+      data: "some kind of data",
     },
   };
 
