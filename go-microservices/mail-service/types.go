4bc854d53541b631deb0d499c6413e0e49179a71
Dmitry White <dmitry.b.white@gmail.com>
2023-08-01 19:31:32 +0100

    Add Go Microservices mail service dependencies

--- go-microservices/mail-service/types.go
+++ go-microservices/mail-service/types.go
@@ -1,8 +1,16 @@
 package main
 
+import (
+	"go-microservices/mail-service/smtp"
+
+	mail "github.com/xhit/go-simple-mail/v2"
+)
+
 type AppConfig struct {
-	PORT int
-	ADDR string
+	PORT     int
+	ADDR     string
+	SMTP     *mail.SMTPClient
+	Services Services
 }
 
 type Routes struct {
@@ -10,7 +18,16 @@ type Routes struct {
 	HEALTH string
 }
 
-type RequestPayload struct{}
+type Services struct {
+	Models smtp.Models
+}
+
+type RequestPayload struct {
+	From    string `json:"from"`
+	To      string `json:"to"`
+	Subject string `json:"subject"`
+	Message string `json:"message"`
+}
 
 type ResponsePayload struct {
 	Error   bool   `json:"error"`
