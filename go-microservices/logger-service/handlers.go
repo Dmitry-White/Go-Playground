937974ebe7b50519f82e2a776318f3ab03f81ffe
Dmitry White <dmitry.b.white@gmail.com>
2023-08-09 18:40:58 +0100

    Refactor Go Microservices logger service RPC connection

--- go-microservices/logger-service/handlers.go
+++ go-microservices/logger-service/handlers.go
@@ -9,6 +9,39 @@ import (
 	"net/rpc"
 )
 
+func (app *AppConfig) listenRPC() {
+	listener, err := net.Listen("tcp", app.ADDR_RPC)
+	if err != nil {
+		log.Fatalln("RPC Server Error:", err)
+	}
+	defer listener.Close()
+
+	log.Printf("RPC Server Listener Address: %+v\n", listener.Addr())
+
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			log.Printf("RPC Server Connection Error: %+v\n", err)
+			continue
+		}
+
+		log.Printf("RPC Server Connection: %+v\n", connection)
+
+		go rpc.ServeConn(connection)
+	}
+}
+
+func (app *AppConfig) handleSetup() error {
+	rpcErr := rpc.Register(app)
+	if rpcErr != nil {
+		return rpcErr
+	}
+
+	go app.listenRPC()
+
+	return nil
+}
+
 func (app *AppConfig) handleWriteLog(resw http.ResponseWriter, req *http.Request) {
 	requestPayload := RequestPayload{}
 
@@ -47,45 +80,12 @@ func (app *AppConfig) handleReadLogs(resw http.ResponseWriter, req *http.Request
 	writeJSON(resw, http.StatusAccepted, responsePayload)
 }
 
-func (app *AppConfig) listenRPC() {
-	listener, err := net.Listen("tcp", app.ADDR_RPC)
-	if err != nil {
-		log.Fatalln("RPC Server Error:", err)
-	}
-	defer listener.Close()
-
-	log.Printf("RPC Server Listener Address: %+v\n", listener.Addr())
-
-	for {
-		connection, err := listener.Accept()
-		if err != nil {
-			log.Printf("RPC Server Connection Error: %+v\n", err)
-			continue
-		}
-
-		log.Printf("RPC Server Connection: %+v\n", connection)
-
-		go rpc.ServeConn(connection)
-	}
-}
-
-func (app *AppConfig) handleSetup() error {
-	rpcErr := rpc.Register(app)
-	if rpcErr != nil {
-		return rpcErr
-	}
-
-	go app.listenRPC()
-
-	return nil
-}
-
 func (app *AppConfig) HandleLogRPC(req LogRPCPayload, res *ResponsePayload) error {
 	requestPayload := RequestPayload{
 		Name: req.Name,
 		Data: req.Data,
 	}
-	entry, err := app.Services.writeRPC(&requestPayload)
+	entry, err := app.Services.write(&requestPayload)
 	if err != nil {
 		log.Println("failed to log", err)
 		return err
