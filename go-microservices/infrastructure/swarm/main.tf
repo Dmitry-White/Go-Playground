8618bfa574be6f1576c0772eefb26d3a80f642d5
Dmitry White <dmitry.b.white@gmail.com>
2023-08-22 22:17:48 +0100

    Add Go Microservices Infrastructure user data

--- go-microservices/infrastructure/swarm/main.tf
+++ go-microservices/infrastructure/swarm/main.tf
@@ -11,6 +11,8 @@ resource "aws_instance" "swarm_node" {
   key_name               = aws_key_pair.swarm_node_key.key_name
   vpc_security_group_ids = [aws_security_group.swarm_node_sg.id]
 
+  user_data = file("./user_data.sh")
+
   tags = {
     Name = each.value.name
   }
