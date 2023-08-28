007e4f1855186a50186b73ef2cb149acfbdb81e9
Dmitry White <dmitry.b.white@gmail.com>
2023-08-28 23:17:18 +0100

    Update Go Microservices Infra Swarm deploy script

--- go-microservices/infrastructure/swarm/deploy.sh
+++ go-microservices/infrastructure/swarm/deploy.sh
@@ -2,6 +2,7 @@
 
 AWS_PROFILE=$1
 MODE=$2
+PLAN_PATH=tfplan
 
 set -e
 
@@ -11,18 +12,26 @@ echo "Init Successul!"
 
 if [ $MODE == "up" ]; then
     echo "Running Terraform Plan..."
-    terraform plan -var="profile=$1"
+    terraform plan \
+        -var="profile=$1" \
+        -out=$PLAN_PATH
     echo "Plan Successul!"
 
     echo "Running Terraform Apply..."
-    terraform apply -var="profile=$1"
+    terraform apply \
+        $PLAN_PATH
     echo "Apply Successul!"
 else
     echo "Running Terraform Plan Destroy..."
-    terraform plan -destroy -var="profile=$1"
+    terraform plan \
+        -destroy \
+        -var="profile=$1" \
+        -out=$PLAN_PATH
     echo "Plan Destroy Successul!"
 
     echo "Running Terraform Apply Destroy..."
-    terraform apply -destroy -var="profile=$1"
+    terraform apply \
+        -destroy \
+        $PLAN_PATH
     echo "Apply Destroy Successul!"
 fi
