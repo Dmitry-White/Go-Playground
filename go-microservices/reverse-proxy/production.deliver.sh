872a1e1d9885f88e7e30fcdf75e25e842ff7eda9
Dmitry White <dmitry.b.white@gmail.com>
2023-08-31 22:06:28 +0100

    Fix Go Microservices Caddy dockerfile

--- go-microservices/reverse-proxy/production.deliver.sh
+++ go-microservices/reverse-proxy/production.deliver.sh
@@ -4,6 +4,7 @@ ACCOUNT=dmitrywhite
 NAME=reverse-proxy-production
 VERSION=1.0.0
 TAG=$ACCOUNT/$NAME:$VERSION
+DOCKER_FILE=production.dockerfile
 
 echo "Tag: $TAG"
 
@@ -12,7 +13,7 @@ docker login
 echo "Logging in Successful!"
 
 echo "Building Docker image..."
-docker build -t $TAG .
+docker build -t $TAG -f $DOCKER_FILE .
 echo "Building Successful!"
 
 echo "Pushing Docker image..."
